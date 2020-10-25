8144dfb76b206beaae774ea2382cf48e2c5d4ac4
insungsong <song22861@naver.com>
2020-10-25 19:04:32 +0900

    sub Server add

--- src/Routes/User/Post/Production/EpisodeUpdate/EpisodeUpdateContainer.js
+++ src/Routes/User/Post/Production/EpisodeUpdate/EpisodeUpdateContainer.js
@@ -53,16 +53,9 @@ export default withRouter((props) => {
     dbS3FileId = data.searchEpisode.s3FileId;
   }
 
-  console.log("episodeId", episodeId);
-  console.log("episodeTitle.value", episodeTitle.value);
-  console.log("episodeDescription.value", episodeDescription.value);
-  console.log("episodeVideoFile.value", episodeVideoFile.value);
-  console.log("dbS3ThumbnailId", dbS3ThumbnailId);
-  console.log("dbS3FileId", dbS3FileId);
-
   const onSubmit = async (e) => {
     if (e === "noChangeBoth") {
-      editEpisodeMutation({
+      await editEpisodeMutation({
         variables: {
           episodeId,
           title: episodeTitle.value,
@@ -131,104 +124,129 @@ export default withRouter((props) => {
     }
   }
 
-  const episodeImgEncodingValue = () => {
-    if (getCookieValue("episodeImgFile") === "") {
+  const isEpisodeImgFile = props.location.search.split("=")[0];
 
-      return decodeURIComponent(getCookieValue("dbEpisodeThumbnail"));
+  let episodeImgFile = "";
+  let videoFile = "";
+  let s3ThumbnailId = "";
+  let s3FileId = "";
+
+  if (isEpisodeImgFile === "videoFile") {
+    if (props.location.search.split("&")[0].split("=")[1] !== undefined && props.location.search.split("&")[1].split("=")[1] !== undefined) {
+      videoFile = props.location.search.split("&")[0].split("=")[1];
+      s3FileId = props.location.search.split("&")[1].split("=")[1];
+    }
+  } else if (isEpisodeImgFile === "episodeImgFile" && props.location.search.split("&").length === 2) {
+    if (props.location.search.split("&")[0].split("=")[1] !== undefined && props.location.search.split("&")[1].split("=")[1] !== undefined) {
+      {
+        episodeImgFile = props.location.search.split("&")[0].split("=")[1];
+        s3ThumbnailId = props.location.search.split("&")[1].split("=")[1];
+      }
     } else {
-
-      return decodeURIComponent(getCookieValue("episodeImgFile"));
+      if (props.location.search.split("&")[0].split("=")[1] !== undefined && props.location.search.split("&")[1].split("=")[1] !== undefined && props.location.search.split("&")[2].split("=")[1] !== undefined && props.location.search.split("&")[3].split("=")[1] !== undefined) {
+        episodeImgFile = props.location.search.split("&")[0].split("=")[1];
+        videoFile = props.location.search.split("&")[1].split("=")[1];
+        s3ThumbnailId = props.location.search.split("&")[2].split("=")[1];
+        s3FileId = props.location.search.split("&")[3].split("=")[1];
+      }
     }
-  };
 
-  const videoFileEncodingValue = () => {
-    if (getCookieValue("videoFile") === "") {
 
-      return decodeURIComponent(getCookieValue("dbEpisodeVideoFile"));
-    } else {
 
-      return decodeURIComponent(getCookieValue("videoFile"));
-    }
-  };
+    const episodeImgEncodingValue = () => {
+      if (episodeImgFile === "") {
+        return decodeURIComponent(getCookieValue("dbEpisodeThumbnail"));
+      } else {
+        return episodeImgFile;
+      }
+    };
 
-  const s3EpisodeImgEncodingValue = () => {
-    if (getCookieValue("s3ThumbnailId") === "") {
-      return getCookieValue("dbS3ThumbnailId");
-    } else {
-      return decodeURIComponent(getCookieValue("s3ThumbnailId"));
-    }
-  };
+    const videoFileEncodingValue = () => {
+      if (videoFile === "") {
+        return decodeURIComponent(getCookieValue("dbEpisodeVideoFile"));
+      } else {
+        return videoFile;
+      }
+    };
 
-  const s3VideoFileEncodingValue = () => {
-    if (getCookieValue("s3FileId") === "") {
-      return getCookieValue("dbS3FileId");
-    } else {
-      return decodeURIComponent(getCookieValue("s3FileId"));
-    }
-  };
+    const s3EpisodeImgEncodingValue = () => {
+      if (s3ThumbnailId === "") {
+        return getCookieValue("dbS3ThumbnailId");
+      } else {
+        return s3ThumbnailId;
+      }
+    };
 
-  const asynAction = async (e) => {
-    if (
-      getCookieValue("episodeTitle") &&
-      getCookieValue("episodeDescription") &&
-      getCookieValue("filterCookie") === "true" &&
-      episodeImgEncodingValue() !== null &&
-      videoFileEncodingValue() !== null &&
-      s3EpisodeImgEncodingValue() !== null &&
-      s3VideoFileEncodingValue() !== null
-    ) {
-      document.cookie = `filterCookie=${false};max-age=1`;
+    const s3VideoFileEncodingValue = () => {
+      if (s3FileId === "") {
+        return getCookieValue("dbS3FileId");
+      } else {
+        return s3FileId;
+      }
+    };
 
-      //이 부분을 만든이유는 localhost:5000에 갔다가 다시 돌아와서 이미지가 보여지다가 동영상만 바꿔치기 하는 수정을 했을때 뮤테이션을 할때 정보값이 망가진다.
-      let s3EpisodeImgEncodingValueFunValue = s3EpisodeImgEncodingValue();
-      let s3ThumbNailUrl = `https://weberyday-test.s3.ap-northeast-2.amazonaws.com/${s3EpisodeImgEncodingValueFunValue}`;
+    const asynAction = async (e) => {
+      if (
+        getCookieValue("episodeTitle") &&
+        getCookieValue("episodeDescription") &&
+        getCookieValue("filterCookie") === "true" &&
+        episodeImgEncodingValue() !== null &&
+        videoFileEncodingValue() !== null &&
+        s3EpisodeImgEncodingValue() !== null &&
+        s3VideoFileEncodingValue() !== null
+      ) {
+        document.cookie = `filterCookie=${false};max-age=1`;
 
-      let s3VideoFileEncodingValueFunValue = s3VideoFileEncodingValue();
-      let s3VideoFileUrl = `https://weberyday-test.s3.ap-northeast-2.amazonaws.com/${s3VideoFileEncodingValueFunValue}`;
+        //이 부분을 만든이유는 localhost:5000에 갔다가 다시 돌아와서 이미지가 보여지다가 동영상만 바꿔치기 하는 수정을 했을때 뮤테이션을 할때 정보값이 망가진다.
+        let s3EpisodeImgEncodingValueFunValue = s3EpisodeImgEncodingValue();
+        let s3ThumbNailUrl = `https://weberyday-test.s3.ap-northeast-2.amazonaws.com/${s3EpisodeImgEncodingValueFunValue}`;
 
-      await editEpisodeMutation({
-        variables: {
-          episodeId,
-          title: getCookieValue("episodeTitle"),
-          description: getCookieValue("episodeDescription"),
+        let s3VideoFileEncodingValueFunValue = s3VideoFileEncodingValue();
+        let s3VideoFileUrl = `https://weberyday-test.s3.ap-northeast-2.amazonaws.com/${s3VideoFileEncodingValueFunValue}`;
 
-          //thumbnail: episodeImgEncodingValue() 해당 부분을 쓰면 에러가 안날때도 있지만 이전의 url값이 thumbnail필드에 저장되어서 데이터를 잃게된다. 즉, s3측에서 가져온 key값(필드로 치면 s3ThumbnailId에 담기는)은 제대로 mutation이 일어나는데 그에반해 thumbNail은 이상한 값을 가지고 있어서 이렇게 하드하게 넣어줌
-          thumbnail: s3ThumbNailUrl,
+        await editEpisodeMutation({
+          variables: {
+            episodeId,
+            title: getCookieValue("episodeTitle"),
+            description: getCookieValue("episodeDescription"),
 
-          //file: videoFileEncodingValue() 해당 부분을 쓰면 에러가 안날떄도 있지만 이전의 url값이 저장된다.
-          file: s3VideoFileUrl,
-          s3ThumbnailId: s3EpisodeImgEncodingValue(),
-          s3FileId: s3VideoFileEncodingValue(),
-          actions: "EDIT"
-        }
-      });
-    }
-  };
-  // asynAction();
-  // deleteAllCookies();
+            //thumbnail: episodeImgEncodingValue() 해당 부분을 쓰면 에러가 안날때도 있지만 이전의 url값이 thumbnail필드에 저장되어서 데이터를 잃게된다. 즉, s3측에서 가져온 key값(필드로 치면 s3ThumbnailId에 담기는)은 제대로 mutation이 일어나는데 그에반해 thumbNail은 이상한 값을 가지고 있어서 이렇게 하드하게 넣어줌
+            thumbnail: s3ThumbNailUrl,
 
-  return (
-    <>
-      {loading ? (
-        <Loader />
-      ) : (
-          <EpisodeUpdatePresenter
-            episodeId={episodeId}
-            postId={postId}
-            episodeTitle={episodeTitle}
-            episodeDescription={episodeDescription}
-            imgBase64={imgBase64}
-            setImgBase64={setImgBase64}
-            episodeThumbnail={episodeThumbnail}
-            videoBase64={videoBase64}
-            setVideoBase64={setVideoBase64}
-            episodeVideoFile={episodeVideoFile}
-            data={data}
-            loading={loading}
-            refetch={refetch}
-            onSubmit={onSubmit}
-          />
-        )}
-    </>
-  );
-});
+            //file: videoFileEncodingValue() 해당 부분을 쓰면 에러가 안날떄도 있지만 이전의 url값이 저장된다.
+            file: s3VideoFileUrl,
+            s3ThumbnailId: s3EpisodeImgEncodingValue(),
+            s3FileId: s3VideoFileEncodingValue(),
+            actions: "EDIT"
+          }
+        });
+      }
+    };
+    asynAction();
+    deleteAllCookies();
+
+    return (
+      <>
+        {loading ? (
+          <Loader />
+        ) : (
+            <EpisodeUpdatePresenter
+              episodeId={episodeId}
+              postId={postId}
+              episodeTitle={episodeTitle}
+              episodeDescription={episodeDescription}
+              imgBase64={imgBase64}
+              setImgBase64={setImgBase64}
+              episodeThumbnail={episodeThumbnail}
+              videoBase64={videoBase64}
+              setVideoBase64={setVideoBase64}
+              episodeVideoFile={episodeVideoFile}
+              data={data}
+              loading={loading}
+              refetch={refetch}
+              onSubmit={onSubmit}
+            />
+          )}
+      </>
+    );
+  });
